24fca2ce8eab3f06c7a949c1b5a95ee83baf61df
mioppong <michaeloppong123@gmail.com>
2019-12-30 13:24:19 -0500

    fixing README, and changing code from python2 to python3

--- Readme.md
+++ Readme.md
@@ -1,3 +1,23 @@
-### Welcome
- -to run this app, download it, and open terminal, type "python filename"
--this should prompt you for min, max....
+# Guess Number Generated
+This is a small python app I made for fun. You pick a min and max. Then guess what the generated int is. When you guess a higher number than the answer, it tells you to pick a smaller number, and if you guess a smaller number than the answer, it tells you to guess higher.
+
+## Getting Started
+* Download files and unzip them
+* open terminal in directory,
+
+## Running program
+open directory these files are in, in the terminal, then 
+```
+python3 filename.py
+```
+then enjoy ;)
+
+## Future Work
+* In the future i hope to a counter to see how long it takes people to guess
+
+## Acknowledgments
+* Python3
+* Random library
+
+## Authors
+* Michael Oppong
--- guess.py
+++ guess.py
@@ -4,17 +4,18 @@ import random
 
 
 def ask_for_input():
-    print "\nPick a number b/w a max and min value and we will say if too high or too low"
+    print ("\nPick a Min and Max")
     minimum = int (input("minimum: " ))
     maximum = int (input("maximum: " ))
     if minimum> maximum:
+        print("Min is supposed to be smaller than Max :/")
         ask_for_input()
     else:
          guess_answer(random.randint(minimum, maximum))
 
 def guess_answer(answer):
     answer = answer
-    user_guess = raw_input("\nguess the number generated: ")
+    user_guess = input("\nguess the number generated: ")
     user_guess = int(user_guess)
 
     if user_guess<answer:
@@ -24,7 +25,7 @@ def guess_answer(answer):
         print("\nguess a lower number")
         guess_answer(answer)
     elif(user_guess == answer):
-        print "Yayy"
+        print ("Yayy")
 
 ask_for_input()
         
